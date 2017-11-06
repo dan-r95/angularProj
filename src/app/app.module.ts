a5cb523a202c91a70b36fc0a9782d833b05ff656
dan_r95 <d.rossburg@googlemail.com>
2017-11-06 02:36:12 +0100

    material design, bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatButtonModule, MatCheckboxModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
